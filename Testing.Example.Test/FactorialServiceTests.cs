a935207c091c4456f76b9ab113d4e2a389bc3ad1
Gixon1 <gixon.gargola@gmail.com>
2021-06-25 15:15:12 -0500

    factorial

---
+++ Testing.Example.Test/FactorialServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.Example.Test
+{
+   public class FactorialServiceTests
+    {
+        [Fact]
+        public void TestFactorialZero(){
+            //inicializar - Arrage
+
+            FactorialService service = new FactorialService();
+
+
+            // Estimular el codigo  o actuar  - Act
+            int result = service.Calculate(0);
+
+            // Resultado se ejecuto   -  Assert
+            Assert.Equal(1, result);
+
+
+        }
+
+        [Fact]
+        public void TestFactorialFive() {
+
+            FactorialService service = new FactorialService();
+
+            int result = service.Calculate(5);
+
+            Assert.Equal(120, result);
+
+
+        }
+
+    }
+}
